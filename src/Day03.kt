6e85c558f2851c78b1f6bbbdf6ba955e458963ad
Magnus Heino <magnus@filur.org>
2021-12-03 09:42:44 +0100

    day03

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,15 +1,46 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return 0
+        var gammaRateBinary = ""
+        input.first().forEachIndexed { index, _ ->
+            gammaRateBinary += input.map { it[index] }.groupBy { it }.maxByOrNull { it.value.size }?.key
+        }
+
+        val gammaRate = convertBinaryToDecimal(gammaRateBinary.toLong())
+        val epsilonRate =
+            convertBinaryToDecimal(gammaRateBinary.map { if (it === '0') '1' else '0' }.joinToString("").toLong())
+
+        return gammaRate * epsilonRate
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        fun rating(input: List<String>, defaultIfEqual: Char): Int {
+            check(listOf('0', '1').contains(defaultIfEqual))
+
+            var candidates = input.toMutableList()
+            input.first().forEachIndexed { index, _ ->
+                candidates = candidates.filter { candidate ->
+                    val least = candidates.map { it[index] }.groupBy { it }
+                        .minByOrNull { it.value.size }?.value
+                    val most = candidates.map { it[index] }.groupBy { it }
+                        .maxByOrNull { it.value.size }?.value
+                    candidate[index] === if (least?.size ?: 0 === most?.size ?: 0) defaultIfEqual else if (defaultIfEqual === '1') most?.first() else least?.first()
+                }.toMutableList()
+                if (candidates.size === 1) {
+                    return convertBinaryToDecimal(candidates.first().toLong())
+                }
+            }
+            throw IllegalStateException("Failed to calculate rating")
+        }
+
+        val oxygen = rating(input, '1')
+        val c02 = rating(input, '0')
+
+        return oxygen * c02
     }
 
-    solvePuzzle("Day03_test", ::part1, 0)
-    solvePuzzle("Day03", ::part1, 0)
+    solvePuzzle("Day03_test", ::part1, 198)
+    solvePuzzle("Day03", ::part1, 3895776)
 
-    solvePuzzle("Day03_test", ::part2, 0)
-    solvePuzzle("Day03", ::part2, 0)
+    solvePuzzle("Day03_test", ::part2, 230)
+    solvePuzzle("Day03", ::part2, 7928162)
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -17,3 +17,18 @@ fun solvePuzzle(input: String, solver: (List<String>) -> Int, expectedResult: In
     println("$input resolved with $solver is $result")
     check(result === expectedResult)
 }
+
+fun convertBinaryToDecimal(num: Long): Int {
+    var num = num
+    var decimalNumber = 0
+    var i = 0
+    var remainder: Long
+
+    while (num.toInt() != 0) {
+        remainder = num % 10
+        num /= 10
+        decimalNumber += (remainder * Math.pow(2.0, i.toDouble())).toInt()
+        ++i
+    }
+    return decimalNumber
+}
