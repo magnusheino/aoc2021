718e914b816753496763fbf930d4942d4759f60c
Magnus Heino <magnus@filur.org>
2021-12-07 06:58:38 +0100

    day 07

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,16 +1,52 @@
+import kotlin.math.abs
+
 fun main() {
 
     fun part1(input: List<String>): Int {
-        return 0
+        val positions = input.first().split(",").map { it.toInt() }
+
+        val min = positions.minOf { it }
+        val max = positions.maxOf { it }
+
+        var bestPosition = 0 to Int.MAX_VALUE
+
+        for (position in min..max) {
+            val cost = positions.sumOf { abs(position - it) }
+            if (cost < bestPosition.second) {
+                bestPosition = position to cost
+            }
+        }
+
+        println("Align to $bestPosition")
+
+
+        return bestPosition.second
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        val positions = input.first().split(",").map { it.toInt() }
+
+        val min = positions.minOf { it }
+        val max = positions.maxOf { it }
+
+        var bestPosition = 0 to Int.MAX_VALUE
+
+        for (position in min..max) {
+            val cost = positions.sumOf { (1..abs(position - it)).sum() }
+            if (cost < bestPosition.second) {
+                bestPosition = position to cost
+            }
+        }
+
+        println("Align to $bestPosition")
+
+
+        return bestPosition.second
     }
 
-    solvePuzzle("Day07_test", ::part1, 0)
-    solvePuzzle("Day07", ::part1, 0)
+    solvePuzzle("Day07_test", ::part1, 37)
+    solvePuzzle("Day07", ::part1, 336721)
 
-    solvePuzzle("Day07_test", ::part2, 0)
-    solvePuzzle("Day07", ::part2, 0)
+    solvePuzzle("Day07_test", ::part2, 168)
+    solvePuzzle("Day07", ::part2, 91638945)
 }
---
+++ src/Day08.kt
@@ -0,0 +1,16 @@
+fun main() {
+
+    fun part1(input: List<String>): Int {
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+    solvePuzzle("Day08_test", ::part1, 0)
+    solvePuzzle("Day08", ::part1, 0)
+
+    solvePuzzle("Day08_test", ::part2, 0)
+    solvePuzzle("Day08", ::part2, 0)
+}
