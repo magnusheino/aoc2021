7f93bf69b74233d2028eaa0dc17bf29fff7f7b6e
Magnus Heino <magnus@filur.org>
2021-12-10 08:52:52 +0100

    day10

--- src/Day10.kt
+++ src/Day10.kt
@@ -1,18 +1,70 @@
-fun main() {
+import java.util.*
 
+fun main() {
+    
+    data class Line(val data: CharArray) {
+        private fun isStartingChar(char: Char): Boolean {
+            return listOf('(', '{', '[', '<').contains(char)
+        }
+
+        private fun getIllegalCharacterPoints(char: Char): Int {
+            return when (char) {
+                ')' -> 3
+                ']' -> 57
+                '}' -> 1197
+                '>' -> 25137
+                else -> throw IllegalStateException("Illegal char $char")
+            }
+        }
+
+        fun getCorruptedPoints(): Int {
+            val startingChars = LinkedList<Char>()
+            data.forEach {
+                if (isStartingChar(it)) {
+                    startingChars.addFirst(it)
+                } else {
+                    when (startingChars.poll()) {
+                        '(' -> if (it != ')') return getIllegalCharacterPoints(it)
+                        '{' -> if (it != '}') return getIllegalCharacterPoints(it)
+                        '<' -> if (it != '>') return getIllegalCharacterPoints(it)
+                        '[' -> if (it != ']') return getIllegalCharacterPoints(it)
+                    }
+                }
+            }
+
+            return 0
+        }
+
+        fun getAutocompletePoints(): Long {
+            val startingChars = LinkedList<Char>()
+            data.forEach {
+                if (isStartingChar(it)) startingChars.addFirst(it) else startingChars.poll()
+            }
+
+            return startingChars.fold(0L) { score, startingChar ->
+                score * 5 + when (startingChar) {
+                    '{' -> 3
+                    '[' -> 2
+                    '<' -> 4
+                    '(' -> 1
+                    else -> throw IllegalStateException("Illegal char $startingChar")
+                }
+            }
+        }
+    }
 
     fun part1(input: List<String>): Int {
-        return 0
+        return input.map { Line(it.toCharArray()) }.sumOf { it.getCorruptedPoints() }
     }
 
-
-    fun part2(input: List<String>): Int {
-        return 0
+    fun part2(input: List<String>): Long {
+        return input.map { Line(it.toCharArray()) }.filter { it.getCorruptedPoints() === 0 }
+            .map { it.getAutocompletePoints() }.sorted().let { (it[it.size / 2] + it[(it.size - 1) / 2]) / 2 }
     }
 
-    solvePuzzle("Day10_test", ::part1, 0)
-    solvePuzzle("Day10", ::part1, 0)
+    solvePuzzle("Day10_test", ::part1, 26397)
+    solvePuzzle("Day10", ::part1, 240123)
 
-    solvePuzzle("Day10_test", ::part2, 0)
-    solvePuzzle("Day10", ::part2, 0)
+    solvePuzzle("Day10_test", ::part2, 288957L)
+    solvePuzzle("Day10", ::part2, 3260812321L)
 }
---
+++ src/Day11.kt
@@ -0,0 +1,18 @@
+fun main() {
+
+
+    fun part1(input: List<String>): Long {
+        return 0L
+    }
+
+
+    fun part2(input: List<String>): Long {
+        return 0L
+    }
+
+    solvePuzzle("Day11_test", ::part1, 0L)
+    solvePuzzle("Day11", ::part1, 0L)
+
+    solvePuzzle("Day11_test", ::part2, 0L)
+    solvePuzzle("Day11", ::part2, 0L)
+}
